b22106017fede0569342ae9cb23ba6613f67c899
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-23 10:48:45 +0100

    move files

--- scripts/update-readme.mjs
+++ scripts/update-readme.mjs
@@ -4,14 +4,14 @@ import path from 'path';
 import PackageJson from '@npmcli/package-json';
 
 // Get the root directory of the repository
-const rootDir = process.cwd();
+const packageRootDir = path.join(process.cwd(), "/packages");
 
-console.log('compiling packages for readme from:', rootDir);
+console.log('compiling packages for readme from:', packageRootDir);
 
 // Get a list of all directories at the root level that contain a package.json file
-const packageDirectories = fs.readdirSync(rootDir, { withFileTypes: true })
+const packageDirectories = fs.readdirSync(packageRootDir, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
-    .map(directory => path.join(rootDir, directory.name))
+    .map(directory => path.join(packageRootDir, directory.name))
     .filter(directory => fs.existsSync(path.join(directory, 'package.json')));
 
 
@@ -27,14 +27,14 @@ await Promise.all(packageDirectories.map(async (dir) => {
 
 // Generate the markdown content
 const mdHeader = `# web-components\n\nThis repository contains a collection of ${count} web components for various purposes. Each component is published as a separate package with their own readme.\n\n---\n\n`;
-const relativeLink = (dir) => "/" + path.relative(rootDir, dir);
+const relativeLink = (dir) => "/" + path.relative(packageRootDir, dir);
 const pkgToMdRow = ([dir, pkgJson]) => `| [${pkgJson?.name ?? '-'}](${relativeLink(dir)}) | ${pkgJson?.version ?? '-'} | ${pkgJson?.description ?? '-'} |`;
 const mdBody = `| Package | Version | Description |\n| --- | --- | --- | \n${Object.entries(pkgDetails).map(pkgToMdRow).join('\n')}\n\n`;
 const mdFooter = `Made with ❤️ by [jackcarey](https://jackcarey.co.uk/)`;
 const markdownContent = `${mdHeader}\n${mdBody}\n${mdFooter}`.trim();
 
 // Write the markdown content to the README.md file
-const readmePath = path.join(rootDir, 'README.md');
+const readmePath = path.join(packageRootDir, 'README.md');
 fs.writeFileSync(readmePath, markdownContent);
 
 console.log(count + ' package details saved to README.md');
