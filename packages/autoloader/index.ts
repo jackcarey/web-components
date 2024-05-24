b2eb09abb717b03585407a0f7ded7cea810fa5d1
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-24 12:48:19 +0100

    add module comment and example

--- packages/autoloader/index.ts
+++ packages/autoloader/index.ts
@@ -1,7 +1,17 @@
+/**
+   * @module
+   * This module automatically loads the components
+   *
++  * @example
++  * ```html
++  * <script src="https://esm.sh/jsr/@web-components/autoloader"></script>
++  * ```
+   */
+
 window.addEventListener("DOMContentLoaded", () => {
-    const components = ["can-i-use","i-cal"];
-    components.forEach(component => {
-        const script = document.createElement('script');
+    const components = ["can-i-use", "i-cal"];
+    components.forEach((component) => {
+        const script = document.createElement("script");
         script.src = `/packages/${component}.js`;
         document.head.appendChild(script);
     });
