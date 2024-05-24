3844f25a6f850639b674afa670baae83c8760eb5
GitHub Action <action@github.com>
2024-05-24 10:06:01 +0000

    [GH Actions] updated readme and/or config files

--- packages/autoloader/index.ts
+++ packages/autoloader/index.ts
@@ -1,17 +1,7 @@
-/**
-   * @module
-   * This module contains automatically loads all components from the JSR @web-components scope
-   *
-+  * @example
-+  * ```html
-+  * <script src="https://esm.sh/jsr/@web-components/autoloader"></script>
-+  * ```
-   */
-
 window.addEventListener("DOMContentLoaded", () => {
-    const components = ["can-i-use", "i-cal"];
-    components.forEach((component) => {
-        const script = document.createElement("script");
+    const components = ["can-i-use","i-cal"];
+    components.forEach(component => {
+        const script = document.createElement('script');
         script.src = `/packages/${component}.js`;
         document.head.appendChild(script);
     });
