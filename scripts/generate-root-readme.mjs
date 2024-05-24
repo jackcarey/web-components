f7492c76f18468023074fa3460e216c9ddbeec7b
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-24 12:32:03 +0100

    update text

--- scripts/generate-root-readme.mjs
+++ scripts/generate-root-readme.mjs
@@ -5,7 +5,7 @@ import { repoRootDir, pkgDetails, count } from "./get-packages.mjs";
 console.log('compiling packages for root readme from:', repoRootDir);
 
 // Generate the markdown content
-const mdHeader = `# web-components\n\n## This repo is currently a work-in-progress.\n\nThis repository contains a collection of ${count} web components for various purposes. Each component is published as a separate package with its own readme. The 'autoloader' can be used to load other components. \n\n`;
+const mdHeader = `# web-components\n\n## This repo is currently a work-in-progress.\n\nThis is a collection of ${count} web components for various purposes. Each component is published as a separate package with its own readme. The 'autoloader' can be used to load other components. \n\n`;
 const relativeLink = (dir) => "/" + path.relative(repoRootDir, dir);
 const pkgToMdRow = ([dir, pkgJson]) => {
     const name = pkgJson?.name ?? '-';
