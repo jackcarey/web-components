71133f5ffa4dee3ecbd0b9507f20d7f485582b3a
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-25 08:30:22 +0100

    add version bumping script

---
+++ scripts/bump-versions.sh
@@ -0,0 +1,8 @@
+echo "bumping package patch versions"
+for dir in $(find "$(git rev-parse --show-toplevel)/packages" -name 'package.json' -not -path '*/node_modules/*' -type f -printf '%h\n' | sort -u)
+do
+    echo "bumping $dir"
+    cd $dir
+    npm version patch -m "Bump package patch version"
+    cd ..
+done
