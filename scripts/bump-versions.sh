6072577f564d5c0662b29709668671a581dbc6c9
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-26 23:03:20 +0100

    refactor find into variable

--- scripts/bump-versions.sh
+++ scripts/bump-versions.sh
@@ -1,8 +1,13 @@
 echo "bumping package patch versions"
-for dir in $(find "$(git rev-parse --show-toplevel)/packages" -name 'package.json' -not -path '*/node_modules/*' -type f -printf '%h\n' | sort -u)
-    do
+directories=$(find "$(git rev-parse --show-toplevel)/packages" -name 'package.json' -not -path '*/node_modules/*' -type f -printf '%h\n' | sort -u)
+for dir in $directories; do
+    cd $dir
+    changes=$(git diff $(git merge-base origin/main HEAD))
+    if [ -z "$changes" ]; then
+        echo "No changes in $dir. Skipping version bump."
+    else
         echo "bumping $dir"
-        cd $dir
         npm version patch -m "Bump package patch version"
-        cd ..
-    done
+    fi
+    cd ..
+done
