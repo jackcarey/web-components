afcbc1b96697f83d834a4d3409e823bf0d7b7a30
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-27 01:05:34 +0100

    Add symbol documentation (#16)
    * add async and fetchPriority attributes to autoloader script
    
    * update cSpell words in VSCode settings.json
    
    * refactor autoloader to only generate package list
    
    * add symbol docs

--- packages/can-i-use/index.ts
+++ packages/can-i-use/index.ts
@@ -15,20 +15,42 @@ import list from "./feature-list";
  * !function(){document.addEventListener("DOMContentLoaded",function(){for(var e=document.getElementsByClassName("ciu_embed"),t=0;t<e.length;t++){var a=e[t],i=a.getAttribute("data-feature"),n=a.getAttribute("data-periods"),s=a.getAttribute("data-accessible-colours")||"false",r=a.getAttribute("data-image-base")||"none";if(i){var o="https://caniuse.bitsofco.de/embed/index.html",d='<iframe src="'+o+"?feat="+i+"&periods="+n+"&accessible-colours="+s+"&image-base="+r+'" frameborder="0" width="100%" height="400px"></iframe>';a.innerHTML=d}else a.innerHTML="A feature was not included. Go to <a href='https://caniuse.bitsofco.de/#how-to-use'>https://caniuse.bitsofco.de/#how-to-use</a> to generate an embed."}var c=window.addEventListener?"addEventListener":"attachEvent";(0,window[c])("attachEvent"==c?"onmessage":"message",function(t){var a=t.data;if("string"==typeof a&&a.indexOf("ciu_embed")>-1)for(var i=a.split(":")[1],n=a.split(":")[2],s=0;s<e.length;s++){var r=e[s];if(r.getAttribute("data-feature")===i){var o=parseInt(n)+30;r.childNodes[0].height=o+"px";break}}},!1)})}();
  */
 
+/**
+ * Represents a custom web component that displays information about browser support for a specific feature.
+ */
 class CanIuseComponent extends HTMLElement {
+    /**
+     * Returns the list of attributes that the component observes for changes.
+     */
     static get observedAttributes(): string[] {
         return ["feature"];
     }
+
     #root: ShadowRoot | undefined;
+
+    /**
+     * Called when the component is connected to the DOM.
+     */
     connectedCallback(): void {
         if (!this.#root) {
             this.#root = this.attachShadow({ mode: "open" });
         }
         this.#render();
     }
+
+    /**
+     * Called when the component is disconnected from the DOM.
+     */
     disconnectedCallback(): void {
         this.#root = undefined;
     }
+
+    /**
+     * Called when an observed attribute has changed.
+     * @param name - The name of the attribute that changed.
+     * @param oldValue - The previous value of the attribute.
+     * @param newValue - The new value of the attribute.
+     */
     attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
         if (oldValue === newValue) return;
         if (name === "feature") {
@@ -38,9 +60,19 @@ class CanIuseComponent extends HTMLElement {
         }
         this.#render();
     }
+
+    /**
+     * Gets the value of the "feature" attribute.
+     * @returns The value of the "feature" attribute, or "custom-elementsv1" if not set.
+     */
     get feature(): string {
         return this.getAttribute("feature") ?? "custom-elementsv1";
     }
+
+    /**
+     * Sets the value of the "feature" attribute.
+     * @param value - The new value for the "feature" attribute.
+     */
     set feature(value: string) {
         if (!value) {
             this.removeAttribute("feature");
@@ -48,9 +80,19 @@ class CanIuseComponent extends HTMLElement {
             this.setAttribute("feature", value);
         }
     }
+
+    /**
+     * Gets the value of the "periods" attribute.
+     * @returns The value of the "periods" attribute, or "future_1,current,past_1,past_2" if not set.
+     */
     get periods(): string {
         return this.getAttribute("periods") ?? "future_1,current,past_1,past_2";
     }
+
+    /**
+     * Sets the value of the "periods" attribute.
+     * @param value - The new value for the "periods" attribute.
+     */
     set periods(value: string) {
         if (!value) {
             this.removeAttribute("periods");
@@ -58,9 +100,19 @@ class CanIuseComponent extends HTMLElement {
             this.setAttribute("periods", value);
         }
     }
+
+    /**
+     * Gets the value of the "accessible-colours" attribute.
+     * @returns The value of the "accessible-colours" attribute, or "false" if not set.
+     */
     get accessibleColours(): string {
         return this.getAttribute("accessible-colours") ?? "false";
     }
+
+    /**
+     * Sets the value of the "accessible-colours" attribute.
+     * @param value - The new value for the "accessible-colours" attribute.
+     */
     set accessibleColours(value: string) {
         if (!value) {
             this.removeAttribute("accessible-colours");
@@ -68,6 +120,7 @@ class CanIuseComponent extends HTMLElement {
             this.setAttribute("accessible-colours", String(value ? true : false));
         }
     }
+
     #render(): void {
         if (!this.#root) return;
         //todo: render the iframe embed
--- packages/query/index.ts
+++ packages/query/index.ts
@@ -21,12 +21,21 @@ const defaultResult: QueryResult = {
     failureCount: 0,
 };
 
+/**
+ * Represents a query object that handles fetching and caching data.
+ */
 class Query {
     #key = "";
     #fetchFn: undefined | FetchFn;
     #options: Options = defaultOptions;
-    //todo: handle global store
     #localResult = defaultResult;
+
+    /**
+     * Creates a new instance of the Query class.
+     * @param key - The key used to identify the query.
+     * @param fetchFn - The function used to fetch the data.
+     * @param options - The options for the query.
+     */
     constructor(key: any, fetchFn: FetchFn, options: Options) {
         this.#key = ["string", "number"].includes(typeof key) ? String(key) : JSON.stringify(key);
         this.#fetchFn = fetchFn;
@@ -38,11 +47,16 @@ class Query {
         this.refetch();
     }
 
+    /**
+     * Gets the key of the query.
+     */
     get key() {
         return this.#key;
     }
 
-    //todo: handle global store
+    /**
+     * Fetches the data for the query.
+     */
     refetch() {
         if (this.#fetchFn) {
             this.#localResult.status = "pending";
@@ -70,10 +84,16 @@ class Query {
         }
     }
 
+    /**
+     * Gets the options of the query.
+     */
     get options() {
         return this.#options;
     }
 
+    /**
+     * Checks if the query is using a global store.
+     */
     get isGlobal() {
         return (
             this.#options?.globalStore &&
@@ -81,6 +101,9 @@ class Query {
         );
     }
 
+    /**
+     * Gets the option values of the query.
+     */
     get optionValues() {
         const opts = { ...defaultOptions, ...this.#options };
         if (typeof this.#options?.retry === "function") {
@@ -95,18 +118,30 @@ class Query {
         return opts as Options;
     }
 
+    /**
+     * Gets the status of the query.
+     */
     get status() {
         return this.#localResult.error ? "error" : this.#localResult.status;
     }
 
+    /**
+     * Checks if the query is pending.
+     */
     get isPending() {
         return this.#localResult.status === "pending";
     }
 
+    /**
+     * Checks if the query has encountered an error.
+     */
     get isError() {
         return this.#localResult.error ? true : false;
     }
 
+    /**
+     * Gets the data of the query.
+     */
     get data() {
         if (this.isStale) {
             this.refetch();
@@ -119,6 +154,9 @@ class Query {
         );
     }
 
+    /**
+     * Checks if the query is using previous data.
+     */
     get isPreviousData() {
         const hasData = this.#localResult.data;
         const hasPrevious = this.#localResult.previousData;
@@ -126,10 +164,16 @@ class Query {
         return !hasData && hasPrevious && !hasInitial;
     }
 
+    /**
+     * Gets the timestamp when the data was last updated.
+     */
     get dataUpdatedAt() {
         return this.#localResult.dataUpdatedAt;
     }
 
+    /**
+     * Checks if the query data is stale.
+     */
     get isStale() {
         if (!this.#localResult.dataUpdatedAt) return true;
         const now = Date.now();
@@ -138,14 +182,23 @@ class Query {
         return now - ttl >= updatedMs;
     }
 
+    /**
+     * Gets the error of the query.
+     */
     get error() {
         return this.#localResult.error;
     }
 
+    /**
+     * Gets the timestamp when the error occurred.
+     */
     get errorUpdatedAt() {
         return this.#localResult.errorUpdatedAt;
     }
 
+    /**
+     * Gets the timestamp when the query was last updated.
+     */
     get updatedAt() {
         if (!this.#localResult.dataUpdatedAt) return this.#localResult.errorUpdatedAt;
         if (!this.#localResult.errorUpdatedAt) return this.#localResult.dataUpdatedAt;
@@ -154,17 +207,27 @@ class Query {
         return new Date(Math.max(dataNumber, errNumber));
     }
 
+    /**
+     * Gets the failure count of the query.
+     */
     get failureCount() {
         return this.#localResult.failureCount;
     }
 
+    /**
+     * Returns a string representation of the query.
+     */
     toString() {
         return JSON.stringify({ ...this.optionValues, ...this.#localResult });
     }
 
+    /**
+     * Returns the data value of the query.
+     */
     valueOf() {
         return this.data;
     }
 }
 
-class StaticQuery {}
+export default Query;
+export { Query };
