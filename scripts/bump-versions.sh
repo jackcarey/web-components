4bfa5e0a7101c65158721897b8ac4f8db03ace9c
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-05-27 01:23:12 +0100

    try version bump diff change

--- scripts/bump-versions.sh
+++ scripts/bump-versions.sh
@@ -2,7 +2,7 @@ echo "bumping package patch versions"
 directories=$(find "$(git rev-parse --show-toplevel)/packages" -name 'package.json' -not -path '*/node_modules/*' -type f -printf '%h\n' | sort -u)
 for dir in $directories; do
     cd $dir
-    changes=$(git diff $(git merge-base origin/main HEAD))
+    changes=$(git diff $(git merge-base origin/main HEAD) origin/main)
     if [ -z "$changes" ]; then
         echo "No changes in $dir. Skipping version bump."
     else
