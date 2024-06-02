82ee322e4aebc73f3b11951fb94e9a64e3f31bf9
Jack Carey <jack.carey@autodesk.com>
2024-06-02 11:28:57 +0100

    save jest results to files

--- scripts/test-json.mjs
+++ scripts/test-json.mjs
@@ -1,14 +1,14 @@
 import fs from "fs";
 import { exec } from "child_process";
 
-exec('jest --json', (error, stdout, stderr) => {  
+exec("jest --json", (_error, stdout, stderr) => {
   //Save test results
-  fs.writeFileSync('test-output/results.json', stdout);
-  console.log('Saved test results to jest-results.json');
+  fs.writeFileSync("test-output/results.json", JSON.stringify(stdout, null, 2));
+  console.log("Saved test results to jest-results.json");
 
   // Save other messages and errors
   if (stderr) {
-    fs.writeFileSync('test-output/errors.log', stderr);
-      console.log('Saved error log to jest-errors.log');
+    fs.writeFileSync("test-output/errors.log", stderr);
+    console.log("Saved error log to jest-errors.log");
   }
 });
