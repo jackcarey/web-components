bc48ad18fc3ce5ac9bf625331d20b9881c768ea9
Jack Carey <jack.carey@autodesk.com>
2024-06-02 11:29:58 +0100

    throw errors

--- scripts/test-json.mjs
+++ scripts/test-json.mjs
@@ -1,14 +1,18 @@
 import fs from "fs";
 import { exec } from "child_process";
 
-exec("jest --json", (_error, stdout, stderr) => {
+exec("jest --json", (_error, stdout, _stderr) => {
+  if(_error){
+    throw new Error(_error);
+  }
+
   //Save test results
   fs.writeFileSync("test-output/results.json", JSON.stringify(stdout, null, 2));
   console.log("Saved test results to jest-results.json");
 
   // Save other messages and errors
-  if (stderr) {
-    fs.writeFileSync("test-output/errors.log", stderr);
-    console.log("Saved error log to jest-errors.log");
-  }
+  // if (stderr) {
+  //   fs.writeFileSync("test-output/errors.log", stderr);
+  //   console.log("Saved error log to jest-errors.log");
+  // }
 });
