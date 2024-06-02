9ff642ed5230e1ff24bb6814611af544d1013228
Jack Carey <jack.carey@autodesk.com>
2024-06-02 15:18:57 +0100

    add minimal flag to reporter config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   reporters: [
-    ['jest-ctrf-json-reporter', {}],
+    ['jest-ctrf-json-reporter', {
+      minimal: true,                  // Optional: Generate a minimal report. Defaults to 'false'. Overrides screenshot and testType when set to true.
+    }]
   ],
 };
