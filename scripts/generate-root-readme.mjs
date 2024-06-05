69697c4fc325e6b00ce8e6e41e7dd8b9b630651e
Jack Carey <jack.carey@autodesk.com>
2024-06-05 14:13:51 +0100

    auto add workflow badges to readme

--- scripts/generate-root-readme.mjs
+++ scripts/generate-root-readme.mjs
@@ -36,9 +36,21 @@ const mdBody = `| Name | Description | Version | License | Registry |\n| --- | -
   .map(pkgToMdRow)
   .join("\n")}\n\n`;
 
+// Find workflow files in .github/workflows directory
+const workflowDir = path.join(repoRootDir, ".github", "workflows");
+const workflowFiles = fs.readdirSync(workflowDir).filter(file => file.endsWith('.yml'));
+
+// Add workflow files to workflowBadges object
+const workflowBadges = workflowFiles.map((file) => {
+  const badgeLink = `https://github.com/jackcarey/web-components/actions/workflows/${file}/badge.svg?branch=main`;
+  const runLink = `https://github.com/jackcarey/web-components/actions/workflows/${file}?query=branch%3Amain)`;
+  return `[![${file} workflow](${badgeLink})](${runLink})`;
+});
+
 const replacements = {
   $count: count,
   $table: mdBody,
+  $workflowBadges: workflowBadges.join("\n"),
 };
 
 const readmeContent = Object.entries(replacements).reduce(
