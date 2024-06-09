eb5a103d0edfa8d635b0a7f0c5f25c1eb9e601d1
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-06-09 16:13:03 +0100

    update stories

--- storybook-docs/can-i-use.stories.ts
+++ storybook-docs/can-i-use.stories.ts
@@ -4,6 +4,33 @@ export default {
     title: "can-i-use",
 };
 
+const defaultArgs = {
+    feature: "custom-elements",
+    periods: "current",
+    accessibleColours: "false",
+    mode: "iframe",
+};
+
 export const Default = () => {
     return document.createElement("can-i-use");
+};
+
+Default.args = defaultArgs;
+
+export const WebPImage = () => {
+    return document.createElement("can-i-use");
+};
+
+WebPImage.args = {
+    ...defaultArgs,
+    feature: "webp",
+};
+
+export const ColorInput = () => {
+    return document.createElement("can-i-use");
+};
+
+ColorInput.args = {
+    ...defaultArgs,
+    feature: "color-input",
 };
