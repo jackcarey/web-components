9aba8e406dc500c1db8392e6fbb6f923a197f94c
Jack Carey <jack.carey@autodesk.com>
2024-07-28 22:37:59 +0100

    update workflows

--- scripts/check-stories.mjs
+++ scripts/check-stories.mjs
@@ -1,6 +1,8 @@
 import fs from 'fs';
-import { repoRootDir, pkgDetails } from "./get-packages.mjs";
+import { repoRootDir, pkgDetails } from './get-packages.mjs';
 
-return !Object.keys(pkgDetails).some(name => {
-    return fs.existsSync(`${repoRootDir}/stories/${name}.stories.ts`);
+return !Object.keys(pkgDetails).some((name) => {
+  const isUtility = !name.includes('-');
+  const path = `${repoRootDir}/stories/${isUtility ? 'utilities' : 'components'}/${name}.stories.ts`;
+  return fs.existsSync(path);
 });
