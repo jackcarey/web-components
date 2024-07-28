3a1b8ee38557dfcd07f6f8fe9fcf03417bdcdee1
Jack Carey <jack.carey@autodesk.com>
2024-07-28 23:06:21 +0100

    improve logging

--- scripts/check-story-results.mjs
+++ scripts/check-story-results.mjs
@@ -1,5 +1,9 @@
 import fs from 'fs';
 
 const result = fs.existsSync(`${process.cwd()}/stories/results.json`);
-
+if(!result) {
+  console.error('No results file');
+}else{
+    console.log('Results file exists');
+}
 process.exit(result ? 0 : 1);
