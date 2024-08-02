81d473b94c1edb5977264aec7f60c5cbabff6f08
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-08-02 15:14:49 +0100

    Fix story check and CI/CD trigger (#38)
    update storybook checks

--- scripts/check-stories.mjs
+++ scripts/check-stories.mjs
@@ -1,12 +1,14 @@
 import fs from 'fs';
 import { repoRootDir, pkgDetails } from './get-packages.mjs';
 
-const allExist = Object.keys(pkgDetails).every((name) => {
+const allExist = Object.values(pkgDetails).every(({name}) => {
   const isUtility = !name.includes('-');
   const path = `${repoRootDir}/stories/${isUtility ? 'utilities' : 'components'}/${name}.stories.ts`;
   const exists = fs.existsSync(path);
-  if(!exists) {
-    console.error(`No stories for ${name}`);
+  if (!exists) {
+    console.error(
+      `No stories for ${isUtility ? 'utility' : 'component'} ${name}`
+    );
   }
   return exists;
 });
