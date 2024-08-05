3338d5d651dcc40ed130af1c97ec5e01204eb8c3
Jack Carey <jack.carey@autodesk.com>
2024-08-05 09:05:30 +0100

    begin stories

--- stories/utilities/datalayer.stories.ts
+++ stories/utilities/datalayer.stories.ts
@@ -1,2 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
 
-export default {};
+const meta: Meta = {};
+export default meta;
+type Story = StoryObj;
+
+const Template = (args: any) => {
+    const randomId = Math.random().toString(36).substring(7);
+    return `
+    <div id="${randomId}"></div>
+    <script>
+    const broadcastLayer = new BroadcastDataLayer("broadcast-layer-key",()=>Math.random(),{ttl:1000});</script>
+    `;
+};
+
+export const Default: Story = Template.bind({});
+Default.args = {};
+
