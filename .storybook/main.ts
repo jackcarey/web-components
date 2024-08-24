fa1bc87ad67871617c566f49c48c2e4c927b33cc
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-08-25 00:51:14 +0100

    chore: Update Storybook configuration in .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,29 +1,28 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
-  stories: [
-    "../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../storybook-docs/**/*.mdx",
-    "../storybook-docs/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/addon-a11y',
-    '@storybook/addon-interactions',
-    '@storybook/addon-themes'
-  ],
-  framework: {
-    name: "@storybook/web-components-vite",
-    options: {},
-  },
-  core: {
-    disableTelemetry: true,
-  },
-  docs: {
-    defaultName: 'All Stories',
-  },
-},
+    stories: [
+        "../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../storybook-docs/**/*.mdx",
+        "../storybook-docs/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../stories/**/*.mdx",
+        "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-a11y",
+        "@storybook/addon-interactions",
+        "@storybook/addon-themes",
+    ],
+    framework: {
+        name: "@storybook/web-components-vite",
+        options: {},
+    },
+    core: {
+        disableTelemetry: true,
+    },
+    docs: {
+        defaultName: "All Stories",
+    },
 };
 export default config;
