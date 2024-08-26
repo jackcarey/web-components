7e8ce65a8d3e97c2f760213e578c456878f7fb5c
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-08-26 18:09:55 +0100

    chore: Update file diff check step in GitHub Actions workflow

---
+++ scripts/commit-push-diffs.mjs
@@ -0,0 +1,26 @@
+const { execSync } = require('child_process');
+
+try {
+    // Get the list of changed files in the latest commit
+    const changedFiles = execSync('git diff --name-only HEAD~1..HEAD').toString().split('\n');
+
+    if (changedFiles.length > 0) {
+        console.log(changedFiles);
+    }
+
+    // Check if any file other than /storybook-static/project.json has changed
+    const hasChangedFiles = changedFiles.some(file => file !== '/storybook-static/project.json');
+
+    if (hasChangedFiles) {
+        console.log('Changes detected. Committing and pushing...');
+        execSync(`git config --global user.email "action@github.com"`);
+        execSync(`git config --global user.name "GitHub Action"`);
+        execSync(`git commit -a -m "[GH Actions] Update documentation and package files" || exit 0`);
+        execSync(`git push`);
+        console.log(`Successfully pushed changes.`);
+    } else {
+        console.log('No changes to be saved.');
+    }
+} catch (error) {
+    console.error('An error occurred:', error);
+}
