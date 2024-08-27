8cfcdc1fa0a0d65fd9ebff5eff0486a3c8f813d9
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-08-27 18:51:48 +0100

    add repo theme

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,8 @@
 import { addons } from "@storybook/manager-api";
+import repoTheme from "./WCTheme";
 
 addons.setConfig({
+    theme: repoTheme,
     sidebar: {
         showRoots: true,
     },
---
+++ .storybook/repoTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+    base: "light",
+    brandTitle: "Web Components",
+    brandUrl: "https://jackcarey.co.uk/web-components",
+    brandImage: "https://jackcarey.co.uk/web-components/images/open-graph.png",
+    brandTarget: "_self",
+});
