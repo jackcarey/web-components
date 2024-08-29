17c54191a509a02b65da718f2e692b87439db26f
[GitHub Action] <action@github.com>
2024-08-29 08:54:57 +0000

    [GH Actions] Update documentation and package files

--- storybook-static/assets/using-components-C1PUNbnj.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as r}from"./index-BSj771as.js";import{ae as i}from"./index-BPfEJMvv.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DzH55vx9.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";function e(n){const o={h1:"h1",p:"p",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Using Components"}),`
-`,t.jsx(o.h1,{id:"using-components",children:"Using Components"}),`
-`,t.jsx(o.p,{children:"//todo"})]})}function f(n={}){const{wrapper:o}={...r(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{f as default};
-//# sourceMappingURL=using-components-C1PUNbnj.js.map
