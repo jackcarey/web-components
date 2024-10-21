a6c18b75a3674d0aab54e14c87eae00c5d54dbee
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-10-21 21:25:01 +0100

    remove repoTheme.ts

--- .storybook/repoTheme.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "@storybook/theming/create";
-
-export default create({
-    base: "light",
-    brandTitle: "Web Components",
-    brandUrl: "https://jackcarey.co.uk/web-components",
-    brandImage: "https://jackcarey.co.uk/web-components/images/open-graph.png",
-    brandTarget: "_self",
-});
