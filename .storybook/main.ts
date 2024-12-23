736a20913e2be2a6d102e61e96f8bc1b432962d1
Jack Carey <jack.carey@autodesk.com>
2024-12-23 21:44:03 +0000

    change base URL path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -51,7 +51,7 @@ const config = {
             optimizeDeps: {
                 include: ["storybook-dark-mode", "ical.js"],
             },
-            base: "./",
+            base: "",
             server: {
                 fs: {
                     strict: false,
