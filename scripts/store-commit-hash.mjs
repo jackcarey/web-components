88dd4fd3457ba47069028c1336013522af5a501d
Jack Carey <jack.carey@autodesk.com>
2024-12-23 20:33:21 +0000

    fix script

--- scripts/store-commit-hash.mjs
+++ scripts/store-commit-hash.mjs
@@ -1,12 +1,13 @@
-import { execSync }from 'child_process's;
+import { execSync }from 'child_process';
 import fs from 'fs';
 import path from 'path';
+import { repoRootDir } from './util-packages.mjs';
 
 // Get the last commit hash
 const commitHash = execSync('git rev-parse HEAD').toString().trim();
 
 // Define the output file path
-const outputPath = path.join(__dirname, '..', '.storybook', 'last-commit-hash.txt');
+const outputPath = path.join(repoRootDir, '/.storybook/last-commit-hash.txt');
 
 // Write the commit hash to the file
 fs.writeFileSync(outputPath, commitHash, 'utf8');
