43b3a89d000b8b9ffdc1be9a18ceedeedbe65920
Jack Carey <jack.carey@autodesk.com>
2024-12-23 19:31:01 +0000

    fix header img path

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -5,7 +5,7 @@ const theme = create({
     base: "light",
     brandTitle: "Web Components",
     brandUrl: "https://jackcarey.co.uk",
-    brandImage: "./images/open-graph.png",
+    brandImage: "../images/open-graph.png",
     brandTarget: "_self",
 
     // Typography
