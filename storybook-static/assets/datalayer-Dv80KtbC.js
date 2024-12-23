613e32a4a33dc9d9eebe9de62b3e73b21f3148d3
[GitHub Action] <action@github.com>
2024-12-23 19:25:01 +0000

    [GH Actions] Update documentation and package files

--- storybook-static/assets/datalayer-Dv80KtbC.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./index-CpnDKoW4.js";import{useMDXComponents as r}from"./index-Bo01w_M0.js";import"./_commonjsHelpers-CqkleIqs.js";function o(e){const t={a:"a",h1:"h1",p:"p",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(t.h1,{id:"datalayer",children:"datalayer"}),`
-`,n.jsxs(t.p,{children:["See ",n.jsx(t.a,{href:"https://github.com/jackcarey/web-components",rel:"nofollow",children:"jackcarey/web-components"})," on GitHub."]})]})}function i(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{i as default};
-//# sourceMappingURL=datalayer-Dv80KtbC.js.map
