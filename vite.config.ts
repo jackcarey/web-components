4675bdad1bc85817b3a5f8c0e628b2d4b986c62b
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-12-27 21:34:14 +0000

    revert vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ export default {
     build: {
         rollupOptions: {
             external: ["**/**/DOCUMENTATION.md", "**/README.md"],
-            output: {
-                assetFileNames: '[name].[ext]', // Preserve original file names
-            }
         },
     },
 };
