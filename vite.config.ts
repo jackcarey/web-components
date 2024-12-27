6b6a84419bd98faa8338c98c03f7348a584396e8
jackcarey <35701383+jackcarey@users.noreply.github.com>
2024-12-27 20:35:21 +0000

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 export default {
     build: {
         rollupOptions: {
-            external: ["ical.js"],
+            external: ["**/**/DOCUMENTATION.md", "**/README.md"],
         },
     },
 };
