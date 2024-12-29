a433bfaf0e5f5ba31664408bd552458200d05026
[GitHub Action] <action@github.com>
2024-12-29 12:32:29 +0000

    [GH Actions] Update documentation and package files

---
+++ storybook-static/assets/component-development-DH1PxA_X.js
@@ -0,0 +1,4 @@
+import{j as t}from"./index-CpnDKoW4.js";import{useMDXComponents as m}from"./index-Bo01w_M0.js";import{ae as r}from"./index-DruSjdg_.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-iJJxeo2K.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-DPS9-N-h.js";import"./index-DrFu-skq.js";function e(n){const o={h1:"h1",p:"p",...m(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Development/Components"}),`
+`,t.jsx(o.h1,{id:"component-development",children:"Component Development"}),`
+`,t.jsx(o.p,{children:"//to-do"})]})}function u(n={}){const{wrapper:o}={...m(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{u as default};
+//# sourceMappingURL=component-development-DH1PxA_X.js.map
