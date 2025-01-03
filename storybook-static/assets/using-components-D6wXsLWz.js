9b769667ace51077aec3f85a578edfb50fa37ad1
[GitHub Action] <action@github.com>
2025-01-03 10:59:34 +0000

    [GH Actions] Update documentation and package files

---
+++ storybook-static/assets/using-components-D6wXsLWz.js
@@ -0,0 +1,4 @@
+import{j as n}from"./index-CpnDKoW4.js";import{useMDXComponents as r}from"./index-Bo01w_M0.js";import{ae as s}from"./index-O48d4e8m.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DSPtk9zR.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-DPS9-N-h.js";import"./index-DrFu-skq.js";function e(t){const o={h1:"h1",p:"p",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Using Components"}),`
+`,n.jsx(o.h1,{id:"using-components",children:"Using Components"}),`
+`,n.jsx(o.p,{children:"//todo"})]})}function h(t={}){const{wrapper:o}={...r(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e(t)}export{h as default};
+//# sourceMappingURL=using-components-D6wXsLWz.js.map
