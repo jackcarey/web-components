1add6f4a769869e014094315458d80223892242b
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-01-05 22:59:10 +0000

    Enhance Signal class documentation with detailed JSDoc comments for methods and properties

--- packages/signal/index.ts
+++ packages/signal/index.ts
@@ -21,6 +21,30 @@ type EmitArgs<T> = { action: "set", prop: string | symbol, oldValue: any, value:
     | { action: "setPrototypeOf", prototype: object | null }
     | { action: "data" };
 
+
+/**
+ * A class that extends `EventTarget` and implements `ProxyHandler` to provide reactive data binding and event dispatching.
+ * 
+ * @template T - The type of the data object being proxied. Defaults to `object`.
+ * 
+ * @extends EventTarget
+ * @implements ProxyHandler<T>
+ * 
+ * @property {T} data - The data object being proxied.
+ * @property {SignalOptions | undefined} options - Optional configuration for the signal.
+ * 
+ * @example
+ * ```typescript
+ *
+ * const signal = new Signal({});
+ * 
+ * signal.addEventListener("signal", (event) => {
+ *   console.log(event.detail);
+ * });
+ * 
+ * signal.foo = "bar"; // Triggers the event
+ * ```
+ */
 export default class Signal<T extends Object = object> extends EventTarget implements ProxyHandler<T> {
     [x: string | number | symbol]: any; //allows for any key to be dynamically defined
     private data: T;
@@ -33,6 +57,14 @@ export default class Signal<T extends Object = object> extends EventTarget imple
         return new Proxy(this.data, this) as unknown as T & Signal<T>;
     }
 
+    /**
+     * Emits a custom event with the specified arguments.
+     *
+     * @template T - The type of the event data.
+     * @param {EmitArgs<T>} args - The arguments to include in the event detail.
+     * @returns {boolean} - Returns `true` if the event was successfully dispatched and not canceled, otherwise `false`.
+     *
+     */
     private emit(args: EmitArgs<T>): boolean {
         const timestamp = Date.now();
         const { bubbles = true, cancelable = true, name = "", useSuffix = false } = this.options ?? {};
@@ -48,6 +80,15 @@ export default class Signal<T extends Object = object> extends EventTarget imple
         return tgtCanContinue ? (superResult ?? true) : false;
     }
 
+    /**
+     * Intercepts property access on the target object.
+     *
+     * @template T - The type of the target object.
+     * @param {T} _target - The target object.
+     * @param {string | symbol} property - The name or symbol of the property being accessed.
+     * @param {any} receiver - The proxy or an object that inherits from the proxy.
+     * @returns {ProxyValue<T>} - The value of the property being accessed.
+     */
     get(_target: T, property: string | symbol, receiver: any): ProxyValue<T> {
         if (property === "addEventListener" || property === "removeEventListener") {
             return Reflect.get(EventTarget.prototype, property, this).bind(this);
@@ -55,6 +96,19 @@ export default class Signal<T extends Object = object> extends EventTarget imple
         return Reflect.get(this.data, property, receiver);
     }
 
+    /**
+     * Sets the value of a property on the target object.
+     *
+     * @param _target - The target object on which to set the property.
+     * @param prop - The name or symbol of the property to set.
+     * @param value - The new value for the property.
+     * @param receiver - The proxy or object that initially received the request.
+     * @returns A boolean indicating whether the property was successfully set.
+     *
+     * @remarks
+     * If the property is successfully set, an event is emitted with the action "set",
+     * the property name, the old value, and the new value.
+     */
     set(_target: T, prop: string | symbol, value: any, receiver: any): boolean {
         const oldValue = Reflect.get(this.data, prop, receiver);
         const isSuccessful = Reflect.set(this.data, prop, value, receiver);
@@ -64,6 +118,18 @@ export default class Signal<T extends Object = object> extends EventTarget imple
         return isSuccessful;
     }
 
+    /**
+     * Deletes a property from the target object.
+     *
+     * @param _target - The target object from which the property will be deleted.
+     * @param prop - The name or symbol of the property to delete.
+     * @returns A boolean indicating whether the property was successfully deleted.
+     *
+     * @template T - The type of the target object.
+     * @remarks
+     * If the property is successfully deleted, an event is emitted with the action "deleteProperty",
+     * the property name, and the old value.
+     */
     deleteProperty(_target: T, prop: string | symbol): boolean {
         const oldValue = Reflect.get(this.data, prop);
         const result = Reflect.deleteProperty(this.data, prop);
@@ -73,6 +139,17 @@ export default class Signal<T extends Object = object> extends EventTarget imple
         return result;
     }
 
+    /**
+     * Defines a new property directly on an object, or modifies an existing property on an object, and returns a boolean indicating whether the operation was successful.
+     *
+     * @param _target - The target object on which to define the property.
+     * @param prop - The name or Symbol of the property to be defined or modified.
+     * @param attributes - The descriptor for the property being defined or modified.
+     * @returns A boolean indicating whether the property was successfully defined.
+     * @remarks
+     * If the property is successfully defined, an event is emitted with the action "defineProperty",
+     * the property name, and the value attributes.
+     */
     defineProperty(_target: T, prop: string | symbol, attributes: PropertyDescriptor): boolean {
         const isSuccessful = Reflect.defineProperty(this.data, prop, attributes);
         if (isSuccessful) {
@@ -81,6 +158,17 @@ export default class Signal<T extends Object = object> extends EventTarget imple
         return isSuccessful;
     }
 
+    /**
+     * Sets the prototype of the target object to the specified prototype.
+     *
+     * @param _target - The target object whose prototype is to be set.
+     * @param prototype - The new prototype object or null.
+     * @returns A boolean indicating whether the prototype was successfully set.
+     *
+     * @emits {Object} - Emits an event with the action "setPrototypeOf" and the new prototype.
+     * @remarks
+     * If the prototype is successfully set, an event is emitted with the action "setPrototypeOf" and the new prototype.
+     */
     setPrototypeOf(_target: T, prototype: object | null): boolean {
         const isSuccessful = Reflect.setPrototypeOf(this.data, prototype);
         if (isSuccessful) {
