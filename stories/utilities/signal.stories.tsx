ed0f316588281fbc2553ea0fc0fee1f53d554c86
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-01-05 21:28:40 +0000

    feat: enhance signal story with optional parameter and new examples

--- stories/utilities/signal.stories.tsx
+++ stories/utilities/signal.stories.tsx
@@ -8,11 +8,14 @@ const meta: Meta = {};
 export default meta;
 type Story = StoryObj;
 
-const signalHtml = (context: StoryContext, s: object) => {
+const signalHtml = (context: StoryContext, s?: object) => {
     const id = `signal-${Math.floor(Math.random() * 1000)}`;
     const cssId = `#${id}`;
-    return html`<p id="${cssId}">Story '${context.name}':</p>
-        <pre>${JSON.stringify(s, null, 2)}</pre>`;
+    return html`<div id="${cssId}"><p>
+            Story '<code>${context.name}</code>' | If the DOM does not update below, check the
+            browser console.
+        </p>
+        <pre><output>${s ? JSON.stringify(s, null, 2) : ""}<output></pre></div>`;
 };
 
 export const Default: Story = {
@@ -39,6 +42,40 @@ export const WithDocumentTarget: Story = {
             s.fizz = "bang";
             s.bang = "fizz";
             s.dispatchEvent(new Event("some random event"));
+            return signalHtml(context, s);
+        },
+    ],
+};
+
+export const LotsOfProperties: Story = {
+    decorators: [
+        (_story, context) => {
+            const s = new Signal({}, { target: document });
+            const listener = (evt) => console.log("listener on signal", evt);
+            s.addEventListener("signal", listener);
+            for (let i = 0; i < 1000; ++i) {
+                s[i] = Math.round(Math.random() * 10000);
+            }
+            return signalHtml(context, s);
+        },
+    ],
+};
+
+export const Interval: Story = {
+    decorators: [
+        (_story, context) => {
+            const s = new Signal({});
+            s.addEventListener("signal", (evt) => console.log("listener on signal", evt));
+            let i = 0;
+            const intervalId = setInterval(() => {
+                const rnd = Math.floor(Math.random() * 10000);
+                const key = `prop${rnd}`;
+                s[i] = performance.now();
+                i += 1;
+            }, 500);
+            setTimeout(() => {
+                clearInterval(intervalId);
+            }, 10000);
             return signalHtml(context, s);
         },
     ],
