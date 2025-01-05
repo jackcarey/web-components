0c4e6278fa6776ba3935c806d26b62b849422ed9
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-01-05 20:05:23 +0000

    update story

--- stories/utilities/signal.stories.tsx
+++ stories/utilities/signal.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/web-components";
+import type { Meta, StoryContext, StoryObj } from "@storybook/web-components";
 import Signal from "../../packages/signal";
 import { html } from "lit";
 
@@ -8,19 +8,26 @@ const meta: Meta = {};
 export default meta;
 type Story = StoryObj;
 
+const signalHtml = (context: StoryContext, s: object) => {
+    const id = `signal-${Math.floor(Math.random() * 1000)}`;
+    const cssId = `#${id}`;
+    return html`<p id="${cssId}">Story '${context.name}':</p>
+        <pre>${JSON.stringify(s, null, 2)}</pre>`;
+};
+
 export const Default: Story = {
     decorators: [
         (_story, context) => {
-            document.addEventListener("signal", (evt) =>
-                console.log(`story signal`, context.name, evt)
+            document.body.addEventListener("signal", (evt) =>
+                console.log(`document story signal`, context.name, evt)
             );
             const s = new Signal({ foo: "bar" });
-            s.addEventListener("signal", (evt) => console.log("sotry listneer", evt));
+            s.addEventListener("signal", (evt) => console.log("listener on signal", evt));
             s.fizz = "bang";
             s.bang = "fizz";
             s.dispatchEvent(new Event("some random event"));
             console.log("story finished", s);
-            return html`${JSON.stringify(s)}`;
+            return signalHtml(context, s);
         },
     ],
 };
