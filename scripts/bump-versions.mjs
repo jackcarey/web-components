0767b08b90833dbd0be56755d8cb4de0b3f36b65
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-01-14 23:21:16 +0000

    move filtering out of loop

--- scripts/bump-versions.mjs
+++ scripts/bump-versions.mjs
@@ -1,29 +1,32 @@
 import { execSync } from 'child_process';
-import { pkgDetails } from './util-packages.mjs';
+import { pkgDetails, repoRootDir } from './util-packages.mjs';
+import path from "path";
 
+const excludedFileNames = ['README.md', 'jsr.json', 'package.json'].map(file => file.toLowerCase());
+const allChanges = execSync(`git diff --name-only HEAD~1`).toString().replaceAll("\\", "/").split('\n').filter(filePath => {
+    const pathLower = filePath.toLowerCase();
+    if (!pathLower.length) return false;
+    const isWithinPkg = pathLower.startsWith('package');
+    if (!isWithinPkg) return false;
+    const isExcludedCompletely = excludedFileNames.includes(pathLower);
+    if (isExcludedCompletely) return false;
+    const isDocumentExcluded = excludedFileNames.some(p => p.endsWith(pathLower));
+    if (isDocumentExcluded) {
+        return pathLength && false;
+    }
+    return true;
+});
+console.log(`Within packages/, excluding: ${excludedFileNames.join(", ")}`);
+console.log(`All relevant changes across all packages:\n\n- ${allChanges.join("\n- ")}\n`);
 
 Object.entries(pkgDetails).forEach(([pkgPath, pkg]) => {
-    const allChangedInPkg = execSync(`git diff --name-only HEAD~1 -- ${pkgPath}`).toString().split('\n');
-    console.log(`All changed for ${pkg.name}:`, allChangedInPkg);
+    const relPath = path.relative(repoRootDir, pkgPath).replaceAll("\\", "/");
 
-    const excludedFileNames = ['README.md', 'jsr.json', 'package.json'].map(file => file.toLowerCase());
-    //keep only the files that we want to watch
-    const changedFiles = allChangedInPkg.filter(filePath => {
-        filePath = filePath.toLowerCase();
-        if (filePath.length === 0) {
-            return false;
-        }
-        if (excludedFileNames.includes(filePath)) {
-            return false;
-        }
-        if (excludedFileNames.some(path => path.endsWith(filePath))) {
-            return false;
-        }
-        return true;
-    });
+    //keep only the files for this package
+    const changedFiles = allChanges.filter(x => x.startsWith(relPath));
     if (changedFiles.length > 0) {
         console.log(`Package '${pkg.name}' has changed files:`);
-        console.log(changedFiles);
+        console.log("-", changedFiles.join("\n"));
         execSync(`cd ${pkgPath}`);
         const bumpResult = execSync(`npm version patch -m "Bump package patch version for ${pkg.name}"`);
         console.log(bumpResult);
