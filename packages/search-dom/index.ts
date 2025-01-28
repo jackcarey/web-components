b29a799a40b9b77a46ef8c920264cbaa8f20bf37
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-01-28 22:36:44 +0000

    wip: first version of search-dom

---
+++ packages/search-dom/index.ts
@@ -0,0 +1,143 @@
+/**
+ * A custom element that wraps an input and filters other DOM elements when its value changes.
+ * @param target ['ul'] - A CSS selector for the parent element containing items.
+ * @param items ['items'] - A CSS selector for items within the parent. Items that have `textContent` that includes the value of the inner `input` element will remain visible. Other items will be hidden.
+ * @param mode ['matchCase'] - How to filter item text content. `normal`/omitted or `matchCase`. Default: `normal`.
+ * 
+ * @example
+ * ```
+ *     <search-dom target="#parent" items=".item">
+ *         <input type="search"/>
+ *     <search-dom>
+ *     <ul id="parent">
+ *      <li class=".item">hello</li>
+ *      <li class=".item">world</li>
+ *     </ul>
+ * ```
+ */
+export default class SearchDOM extends HTMLElement implements EventTarget {
+    static observedAttributes = ["target", "items", "mode"];
+    #mutationObserver: MutationObserver | null;
+    #inputListener: EventListener = () => {
+        const tgtSelector = this.getAttribute("target");
+        const itemSelector = this.getAttribute("items");
+        if (tgtSelector && itemSelector) {
+            this.dispatchEvent(new CustomEvent("search-dom", {
+                detail: {
+                    type: "input",
+                    value: this.#inputEl?.value
+                }
+            }));
+            const inputVal = this.#inputEl?.value;
+            document.querySelectorAll(tgtSelector).forEach(tgtEl => {
+                tgtEl.querySelectorAll(itemSelector).forEach(itemEl => {
+                    this.dispatchEvent(new CustomEvent("search-dom", {
+                        detail: {
+                            type: "target",
+                            value: tgtEl
+                        }
+                    }));
+                    const hasInputVal = Boolean(inputVal?.length);
+                    const testContent = this.mode === "matchCase" ? itemEl.textContent : itemEl.textContent?.toLowerCase();
+                    const testVal = this.mode === "matchCase" ? inputVal : inputVal?.toLowerCase();
+                    const hasMatch = Boolean(testContent?.includes(String(testVal)));
+                    const isHidden = hasInputVal && !hasMatch;
+                    itemEl.ariaHidden = String(isHidden);
+                    const currentDisplayValue = getComputedStyle(itemEl).display;
+                    const storedDisplayValue = (itemEl as HTMLElement).dataset['sdDisplay'];
+                    if (isHidden) {
+                        //hide the element
+                        (itemEl as HTMLElement).dataset['sdDisplay'] = currentDisplayValue;
+                        (itemEl as HTMLElement).style.display = 'none';
+                    } else {
+                        //unhide the element
+                        const setTo = storedDisplayValue ?? 'inherit';
+                        (itemEl as HTMLElement).style.display = setTo;
+                    }
+                    this.dispatchEvent(new CustomEvent("search-dom", {
+                        detail: {
+                            type: "item",
+                            value: this.#inputEl?.value,
+                            target: itemEl,
+                            isHidden
+                        }
+                    }));
+                });
+            });
+        }
+    };
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.#mutationObserver?.disconnect();
+        this.#mutationObserver = new MutationObserver((_) => {
+            this.#setUpInputListener();
+        });
+        this.#mutationObserver.observe(this);
+        this.#setUpInputListener();
+    }
+
+    disconnectedCallback() {
+        this.#mutationObserver?.disconnect();
+    }
+
+    attributeChangedCallback() {
+        this.#setUpInputListener();
+    }
+
+    get #inputEl() {
+        return (this.querySelector("input[type='search']") || this.querySelector("input")) as HTMLInputElement | undefined;
+    }
+
+    #setUpInputListener() {
+        if (this.#inputEl) {
+            this.#inputEl.addEventListener("input", this.#inputListener);
+        }
+    }
+
+    run() {
+        this.#inputListener(new Event("input", {}));
+    }
+
+    get target() {
+        return this.getAttribute("target");
+    }
+
+    set target(val: string | undefined | null) {
+        if (val) {
+            this.setAttribute("target", val);
+        } else {
+            this.removeAttribute("target");
+        }
+    }
+
+    get items() {
+        return this.getAttribute("target");
+    }
+
+    set items(val: string | undefined | null) {
+        if (val) {
+            this.setAttribute("items", val);
+        } else {
+            this.removeAttribute("items");
+        }
+    }
+
+    get mode() {
+        return this.getAttribute("mode") ?? "normal";
+    }
+
+    set mode(val: string | undefined | null) {
+        const validVal = val && ["normal", "matchCase"].includes(val) ? val : undefined;
+        if (validVal?.length) {
+            this.setAttribute("mode", validVal);
+        } else {
+            this.removeAttribute("mode");
+        }
+    }
+}
+
+customElements.define("search-dom", SearchDOM);
