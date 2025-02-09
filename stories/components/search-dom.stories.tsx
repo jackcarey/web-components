3fb85e5fdc8588122d2d0588bd4fa80d70e69675
Jack Carey <jack.carey@autodesk.com>
2025-02-09 18:34:29 +0000

    add matchCase story

--- stories/components/search-dom.stories.tsx
+++ stories/components/search-dom.stories.tsx
@@ -12,13 +12,7 @@ export const Default: Story = {
     args: {},
 };
 
-export const Demo: Story = {
-    args: {},
-    render: (_args) => {
-        return html`<search-dom target="ul" items="li">
-        <input type="search"/>
-        </search-dom>
-        <ul>
+const ulHtml = html`<ul>
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
@@ -39,6 +33,51 @@ export const Demo: Story = {
             <li>Item 18</li>
             <li>Item 19</li>
             <li>Item 20</li>
+            <li>Item A</li>
+            <li>Item B</li>
+            <li>Item C</li>
+            <li>Item D</li>
+            <li>Item E</li>
+            <li>Item F</li>
+            <li>Item G</li>
+            <li>Item H</li>
+            <li>Item I</li>
+            <li>Item J</li>
+            <li>Item K</li>
+            <li>Item L</li>
+            <li>Item M</li>
+            <li>Item N</li>
+            <li>Item O</li>
+            <li>Item P</li>
+            <li>Item Q</li>
+            <li>Item R</li>
+            <li>Item S</li>
+            <li>Item T</li>
+            <li>Item U</li>
+            <li>Item V</li>
+            <li>Item W</li>
+            <li>Item X</li>
+            <li>Item Y</li>
+            <li>Item Z</li>
+
         </ul>`;
+
+export const Demo: Story = {
+    args: {},
+    render: (_args) => {
+        return html`<search-dom target="ul" items="li">
+        <input type="search"/>
+        </search-dom>
+        ${ulHtml}`;
+    }
+};
+
+export const MatchCase = {
+    args: {},
+    render: (_args) => {
+        return html`<search-dom target="ul" items="li" mode="matchCase">
+        <input type="search"/>
+        </search-dom>
+        ${ulHtml}`;
     }
 };
