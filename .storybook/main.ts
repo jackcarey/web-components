846c290504bc721dba5a83ea8a203dd89a639fa9
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-02-09 09:38:16 +0000

    wip: Add Chromatic integration and update dependencies in CI/CD workflow

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -29,6 +29,7 @@ const config: StorybookConfig = {
             options: coverageConfig,
         },
         './addons/commitLabel.ts',
+        "@chromatic-com/storybook"
     ],
     framework: {
         name: "@storybook/web-components-vite",
