516e86b47943ed51c298592b445b7cbb47332fae
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-07-27 17:49:22 +0100

    diff-text: add story examples

--- stories/components/diff-text.stories.tsx
+++ stories/components/diff-text.stories.tsx
@@ -24,7 +24,12 @@ const renderFn: StoryObj["render"] = (args: Args) => {
         <p id="changed">Hello World (changed span)</p>
         <b>Diff Text Component:</b>
         <p>
-            <diff-text original="${args.original}" changed="${args.changed}"></diff-text>
+            <diff-text
+                original="${args.original}"
+                changed="${args.changed}"
+                original-src="${args["original-src"]}"
+                changed-src="${args["changed-src"]}"
+            ></diff-text>
         </p>
         <b>Args:</b><br />
         <pre>${JSON.stringify(args, null, 2)}</pre>`;
@@ -50,4 +55,29 @@ type Story = StoryObj;
 
 export const Default: Story = {
     args: { original: "#original", changed: "#changed" },
+};
+
+export const OriginalSrc: Story = {
+    args: {
+        "original-src":
+            "https://gist.githubusercontent.com/jackcarey/8ba2b5b02fb1371171e268f4a557e7bf/raw/0ea3c70a4ecc6183c6a63c820eabfc15a4c97c71/text.txt",
+        changed: "#changed",
+    },
+};
+
+export const ChangedSrc: Story = {
+    args: {
+        original: "#original",
+        "changed-src":
+            "https://gist.githubusercontent.com/jackcarey/8ba2b5b02fb1371171e268f4a557e7bf/raw/0ea3c70a4ecc6183c6a63c820eabfc15a4c97c71/text.txt",
+    },
+};
+
+export const OriginalAndChangedSrc: Story = {
+    args: {
+        "original-src":
+            "https://gist.githubusercontent.com/jackcarey/8ba2b5b02fb1371171e268f4a557e7bf/raw/0ea3c70a4ecc6183c6a63c820eabfc15a4c97c71/text.txt",
+        "changed-src":
+            "https://gist.githubusercontent.com/jackcarey/d8098b3784dcce32f9a4266c89bc1148/raw/abb8bb4d96fadb307997bf1592283b727910f798/other-text.txt",
+    },
 };
