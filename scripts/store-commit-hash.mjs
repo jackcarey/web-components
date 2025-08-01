8fc0e1790eef6fd6a9ab802096f48af7a16672bb
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-08-01 13:05:15 +0100

    Update commit hash retrieval to target jsr.json files and improve log messages for clarity

--- scripts/store-commit-hash.mjs
+++ scripts/store-commit-hash.mjs
@@ -1,10 +1,10 @@
-import { execSync }from 'child_process';
+import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import { repoRootDir } from './util-packages.mjs';
 
-// Get the latest commit hash that contains changes in the packages folder
-const commitHash = execSync('git log -n 1 --pretty=format:%H -- packages', { cwd: repoRootDir }).toString().trim();
+// Get the latest commit hash that contains changes to any jsr.json file
+const commitHash = execSync("git log -n 1 --pretty=format:%H -- '**/jsr.json'", { cwd: repoRootDir }).toString().trim();
 
 // Define the output file path
 const outputPath = path.join(repoRootDir, '/.storybook/last-commit-hash.txt');
@@ -12,7 +12,7 @@ const outputPath = path.join(repoRootDir, '/.storybook/last-commit-hash.txt');
 // Write the commit hash to the file if the file contents would change
 if (!fs.existsSync(outputPath) || fs.readFileSync(outputPath, 'utf8') !== commitHash) {
     fs.writeFileSync(outputPath, commitHash, 'utf8');
-    console.log(`Last commit hash for packages/ folder (${commitHash}) saved to ${outputPath}`);
-}else{
-    console.log(`Last commit hash for packages/ folder (${commitHash}) already saved to ${outputPath}`);
+    console.log(`Last commit hash for jsr.json files (${commitHash}) saved to ${outputPath}`);
+} else {
+    console.log(`Last commit hash for jsr.json files (${commitHash}) already saved to ${outputPath}`);
 }
