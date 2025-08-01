757e4175e62a3702b1ee24bfe9cf2cf9708d0043
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-08-01 13:21:42 +0100

    add package versions to documentation file

--- scripts/generate-autoloader-list.mjs
+++ scripts/generate-autoloader-list.mjs
@@ -11,13 +11,21 @@ Object.values(pkgDetails).forEach(pkg => {
 });
 
 const autoloaderPath = `${repoRootDir}/packages/autoloader/components.ts`;
+const autoLoaderDocsPath = `${repoRootDir}/packages/autoloader/DOCUMENTATION.md`;
 const existingContent = fs.existsSync(autoloaderPath) ? fs.readFileSync(autoloaderPath, 'utf8') : '';
 const newAutoLoaderContent = `const components = ${JSON.stringify(componentVersions)};\nexport default components;`;
+const newDocsContent = `The 'autoloader' has no options.
+Component script tags are added to the head of the document using the [esm.sh CDN](https://esm.sh/). 
+This is done when the components are first seen in the DOM and when the DOM is updated.
+Utilities are not loaded with the autoloader, only DOM components.
+DOM components are are versioned to match each autoloader release.
+This version includes the following components:\n${Object.entries(componentVersions).map(([name, version]) => `\n- \`${name}\`: v${version}`).join('')}\n\n`;
 
 const hasContentChanged = !!(existingContent?.length) || existingContent !== newAutoLoaderContent;
 
 if (hasContentChanged) {
     fs.writeFileSync(autoloaderPath, newAutoLoaderContent);
+    fs.writeFileSync(autoLoaderDocsPath, newDocsContent);
     console.log('Autoloader list updated', newAutoLoaderContent);
 } else {
     console.log('No changes to autoloader list');
