a113f0bd8f8ca8e63701fe1d7dccc2bf69e74a79
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-08-08 17:36:53 +0100

    focus on kbd element after triggering click event

--- packages/kbd-click/index.ts
+++ packages/kbd-click/index.ts
@@ -62,6 +62,7 @@ class KBDClick extends HTMLElement {
                     cancelable: true,
                     composed: true,
                 }));
+                kbdEl.focus();
             }
         });
     }
