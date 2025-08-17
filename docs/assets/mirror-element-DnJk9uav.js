ec43787f0dbe1a44a89e976b37c33b31b6342e28
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-08-17 18:37:15 +0100

    feat: Add new signal stories and documentation
    - Introduced new signal stories in `signal.stories-Dwa_XrZ3.js` showcasing various use cases including default behavior, document target, and interval updates.
    - Added source maps for the new signal stories for better debugging.
    - Created a new utility file `utils-DEQ7wb9-.js` to manage dynamic imports and component decorators.
    - Updated iframe HTML to reference the new assets and adjusted import path matchers for better compatibility.
    - Modified project configuration in `project.json` to reflect recent changes and updates.
    - Updated package dependencies, specifically downgrading `ical.js` from version 2.2.1 to 2.0.1 for compatibility reasons.

--- docs/assets/mirror-element-DnJk9uav.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./index-Dlwx_-PB.js";import{useMDXComponents as e}from"./index-BM3H6bQe.js";import{M as m,a as s}from"./blocks-DmHy35JU.js";import{D as i}from"./README-DiLqPiU6.js";import"./_commonjsHelpers-CqkleIqs.js";import"./preload-helper-Dp1pzeXC.js";import"./iframe-BpB84oCe.js";function r(o){return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"components/mirror-element/Documentation"}),`
-`,t.jsx(s,{children:i})]})}function M(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r()}export{M as default};
-//# sourceMappingURL=mirror-element-DnJk9uav.js.map
