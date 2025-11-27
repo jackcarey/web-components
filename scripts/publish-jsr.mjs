6f02069a8ba7d7098a84b7691781e37618fa62ae
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-11-27 21:04:35 +0000

    fix: filter publishing logic to autoloader for GitHub Actions

--- scripts/publish-jsr.mjs
+++ scripts/publish-jsr.mjs
@@ -13,15 +13,29 @@ console.log('Dry runs complete!');
 console.log('-'.repeat(80));
 
 const branch = logExecSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+const lastCommitMsg = logExecSync('git log -1 --pretty=%B').toString().trim();
+const isGHAction = lastCommitMsg.startsWith('[GH Action]');
 
 if (branch === 'main') {
-    console.log('Publishing packages to JSR...');
-    Object.entries(pkgDetails).forEach(([dir, pkg]) => {
-        if (!pkg.private) {
-            const publishCmd = `cd "${dir}"\nnpx jsr publish --allow-dirty`;
-            logExecSync(publishCmd);
-        }
-    });
+    if (isGHAction) {
+        console.log('GitHub Action - only publishing auto-loader to JSR...');
+        Object.entries(pkgDetails).forEach(([dir, pkg]) => {
+            if (!pkg.private && pkg.name.includes('autoloader')) {
+                const publishCmd = `cd "${dir}"\nnpx jsr publish --allow-dirty`;
+                logExecSync(publishCmd);
+            } else {
+                console.log(`Skipping, package '${pkg.name}' not an autoloader or marked as private`);
+            }
+        });
+    } else {
+        console.log('Publishing packages to JSR...');
+        Object.entries(pkgDetails).forEach(([dir, pkg]) => {
+            if (!pkg.private) {
+                const publishCmd = `cd "${dir}"\nnpx jsr publish --allow-dirty`;
+                logExecSync(publishCmd);
+            }
+        });
+    }
     console.log('Publish complete!');
 } else {
     console.log('Not on main branch, skipping publish.');
