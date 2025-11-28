e8b373bd4de7c2d22ee5ea3c635b4a31be3534c9
jackcarey <35701383+jackcarey@users.noreply.github.com>
2025-11-28 09:35:10 +0000

    fix: enhance bump-versions script to conditionally bump package versions based on branch and commit message

--- scripts/bump-versions.mjs
+++ scripts/bump-versions.mjs
@@ -8,37 +8,54 @@ import fs from 'fs';
 const excludedFileNames = ['README.md', 'jsr.json', 'package-lock.json', 'package.json'].map(file => file.toLowerCase());
 const lastPackageChangesCommit = fs.readFileSync(lastCommitHashFilePath, 'utf8').trim();
 const lastCommitMsg = execSync(`git log -1 --pretty=%B ${lastPackageChangesCommit}`).toString().trim();
-console.log(`Looking for changes since commit: ${lastPackageChangesCommit} - "${lastCommitMsg}"`);
-const allChanges = execSync(`git diff ${lastPackageChangesCommit} HEAD --name-only`).toString().replaceAll("\\", "/").split('\n');
 
-const relevantChanges = allChanges.filter(filePath => {
-    const pathLower = filePath?.toLowerCase();
 
-    if (!pathLower) {
-        return false;
-    }
+const branch = logExecSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+const isMain = branch === 'main';
+const isGHAction = lastCommitMsg.startsWith('[GH Actions]');
 
-    const isWithinPkg = pathLower.startsWith('packages/');
-    const isPathExcludedCompletely = excludedFileNames.includes(pathLower);
-    const isFileNameExcluded = excludedFileNames.some(p => pathLower.endsWith(p));
+if (isMain) {
+    console.log(`Looking for changes since commit: ${lastPackageChangesCommit} - "${lastCommitMsg}"`);
+    const allChanges = execSync(`git diff ${lastPackageChangesCommit} HEAD --name-only`).toString().replaceAll("\\", "/").split('\n');
 
-    return isWithinPkg && !isPathExcludedCompletely && !isFileNameExcluded;
-});
+    const relevantChanges = allChanges.filter(filePath => {
+        const pathLower = filePath?.toLowerCase();
 
-const changedPackages = Array.from(new Set(relevantChanges.map(filePath => {
-    return filePath.split('/')[1];
-})));
-console.log(`Changed packages:\n\n- ${changedPackages.join("\n- ")}\n`);
+        if (!pathLower) {
+            return false;
+        }
 
-Object.entries(pkgDetails).filter(([_, pkg]) => {
-    return changedPackages.includes(pkg.name);
-}).forEach(([pkgPath, pkg]) => {
-    try {
-        process.chdir(pkgPath);
-        const bumpResult = execSync(`npm version patch -m "Bump package patch version for ${pkg.name}" --no-git-tag-version`);
-        console.log(bumpResult.toString());
-    } catch (e) {
-        console.error(`Failed to bump version for package: ${pkg.name}`);
-        console.error(e);
-    }
-});
+        const isWithinPkg = pathLower.startsWith('packages/');
+        const isPathExcludedCompletely = excludedFileNames.includes(pathLower);
+        const isFileNameExcluded = excludedFileNames.some(p => pathLower.endsWith(p));
+
+        return isWithinPkg && !isPathExcludedCompletely && !isFileNameExcluded;
+    });
+
+    const changedPackages = Array.from(new Set(relevantChanges.map(filePath => {
+        return filePath.split('/')[1];
+    })));
+    console.log(`Changed packages:\n\n- ${changedPackages.join("\n- ")}\n`);
+
+    Object.entries(pkgDetails).filter(([_, pkg]) => {
+        const hasChanged = changedPackages.includes(pkg.name);
+        const isAutoloader = pkg.name.includes('autoloader');
+        if (isGHAction) {
+            return hasChanged && isAutoloader;
+        } else {
+            return hasChanged;
+        }
+    }).forEach(([pkgPath, pkg]) => {
+        try {
+            console.log(`Bumping patch version for package: ${pkg.name}`);
+            process.chdir(pkgPath);
+            const bumpResult = execSync(`npm version patch -m "Bump package patch version for ${pkg.name}" --no-git-tag-version`);
+            console.log(bumpResult.toString());
+        } catch (e) {
+            console.error(`Failed to bump version for package: ${pkg.name}`);
+            console.error(e);
+        }
+    });
+} else {
+    console.log('Not on main branch, skipping version bump.');
+}
